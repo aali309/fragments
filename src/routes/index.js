f1532b7845bfb39d4beda043911510c0bbe68b12
Atif Ali <aali309@myseneca.ca>
2023-09-17 11:01:42 -0400

    added last part after endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 // src/routes/index.js
 
 const express = require('express');
+// Our authentication middleware
+const { authenticate } = require('../auth');
 
 // version and author from package.json
 const { version, author } = require('../../package.json');
@@ -10,9 +12,9 @@ const router = express.Router();
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
+ * Protect them all so you have to be authenticated in order to access.
  */
-router.use(`/v1`, require('./api'));
-
+router.use(`/v1`, authenticate(), require('./api'));
 /**
  * Define a simple health check route. If the server is running
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
