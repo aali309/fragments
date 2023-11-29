28c355598e052e3cae6d731e04888b3ed4e274e3
Atif Ali <aali309@myseneca.ca>
2023-11-29 15:56:20 -0500

    lab10 updated unit tests:pass locally

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -4,7 +4,22 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
-describe('404 middleware', () => {
-  test('wrong routes are forwarded to 404 error!', () =>
-    request(app).get('/fragments').expect(404));
+describe('404 Handler', () => {
+  it('should return a 404 error response', (done) => {
+    request(app)
+      .get('/nonexistent-route')
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .expect({
+        status: 'error',
+        error: {
+          message: 'not found',
+          code: 404,
+        },
+      })
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
 });
